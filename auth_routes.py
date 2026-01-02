ad96f1ad5a04fda3909192e30a288624b1d6d0f7
Gustavo Borges de Lima <gustavoblima306@gmail.com>
2026-01-02 16:06:01 -0300

    feat: add create_account route

--- auth_routes.py
+++ auth_routes.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import User
+from dependencies import get_session
+from main import bcrypt_context
+from schemas import UserSchema
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
 
 @auth_router.get('/')
-async def authenticate():
-    return {'message': 'You accessed authenticate route'}
+async def home():
+    return {'message': 'You accessed authenticate route', 'authentificated': False}
+
+
+@auth_router.post('/create_account')
+async def create_account(user_schema: UserSchema, session = Depends(get_session)):
+    user = session.query(User).filter(User.email==user_schema.email).first()
+    if user:
+        # There is already a user with this email address
+        raise HTTPException(status_code=400, detail="Email already registered")
+    else:
+        encrypted_psw = bcrypt_context.hash(user_schema.password)
+        new_user = User(user_schema.name, user_schema.email, encrypted_psw, user_schema.status, user_schema.admin)
+        session.add(new_user)
+        session.commit()
+        return {'message': 'User successfully registered'}
