47df24ca6a755d04d4e6cb20b522abff3b3c6c6e
Gustavo Borges de Lima <gustavoblima306@gmail.com>
2026-01-03 23:48:05 -0300

    feat: add OrderSchema

--- schemas.py
+++ schemas.py
@@ -11,3 +11,10 @@ class UserSchema(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class OrderSchema(BaseModel):
+    user: int
+
+    class Config:
+        from_attributes = True
