317b885d3055d16aec24a152485cca2ded0ecb62
Gustavo Borges de Lima <gustavoblima306@gmail.com>
2026-01-03 23:46:57 -0300

    feat: add create order route

--- order_routes.py
+++ order_routes.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import get_session
+from schemas import OrderSchema
+from models import Order
 
 order_router = APIRouter(prefix='/orders', tags=['orders'])
 
 @order_router.get('/')
 async def orders():
     return {'message': 'You accessed order route'}
+
+
+@order_router.post('/order')
+async def create_order(order_schema: OrderSchema, session = Depends(get_session)):
+    new_order = Order(user=order_schema.user)
+    session.add(new_order)
+    session.commit()
+    return {'message': f'New order successfully created. Order ID: {new_order.id}'}
