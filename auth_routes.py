962bcc5843eb4154cc27f69337889f3ea48fc80a
Gustavo Borges de Lima <gustavoblima306@gmail.com>
2026-01-04 22:08:45 -0300

    feat: add login route

--- auth_routes.py
+++ auth_routes.py
@@ -2,9 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context
-from schemas import UserSchema
+from schemas import UserSchema, LoginSchema
 
 auth_router = APIRouter(prefix='/auth', tags=['auth'])
+
+def create_token(user_id):
+    token = f"dhkn321kn32o1{user_id}"
+    return 
 
 @auth_router.get('/')
 async def home():
@@ -23,3 +27,16 @@ async def create_account(user_schema: UserSchema, session = Depends(get_session)
         session.add(new_user)
         session.commit()
         return {'message': 'User successfully registered'}
+    
+
+@auth_router.post('/login')
+async def login(login_schema: LoginSchema, session = Depends(get_session)):
+    user = session.query(User).filter(User.email==login_schema.email).first()
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found")
+    else:
+        access_token = create_token(user.id)
+        return {
+            "access_token": access_token,
+            "token_type": 'Bearer'
+            }
